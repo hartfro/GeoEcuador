f55085210050b4fb508312decbb7a6a4c6a84b92
Anthony Suárez <neolight1010@gmail.com>
2021-01-02 13:47:14 -0500

    Integrated polygons to main scene.

--- src/provincia/provincia.gd
+++ src/provincia/provincia.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+export(String) var province_name
 export(PackedScene) var Polygon
 
 
@@ -7,9 +8,12 @@ func _ready():
 	if Polygon:
 		remove_child($CollisionShape2D)
 		add_child(Polygon.instance())
+		
+	if not province_name:
+		province_name = name
 
 
 func _on_Provincia_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT \
 			&& event.pressed:
-		print("Just pressed " + name)
+		print("Just pressed " + province_name)
