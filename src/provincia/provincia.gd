b3502d70f716fc24545a535943e02bdf7d972354
Anthony Suárez <neolight1010@gmail.com>
2021-01-02 19:30:57 -0500

    Added InfoLabel to Province.

--- src/provincia/provincia.gd
+++ src/provincia/provincia.gd
@@ -2,18 +2,41 @@ extends Area2D
 
 export(String) var province_name
 export(PackedScene) var Polygon
+export(NodePath) var InfoLabel
 
+var mouse_over = false
 
 func _ready():
+	# Set default Script Variables.
 	if Polygon:
 		remove_child($CollisionShape2D)
 		add_child(Polygon.instance())
 		
 	if not province_name:
 		province_name = name
+		
+	if not InfoLabel or not InfoLabel is Label:
+		InfoLabel = Label.new()
+		add_child(InfoLabel)
+		InfoLabel.text = province_name
+		
+	InfoLabel.hide()
 
 
 func _on_Provincia_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT \
 			&& event.pressed:
-		print("Just pressed " + province_name)
+		InfoLabel.show()
+
+func _input(event):
+	# Handle press outside of Province.
+	if (event is InputEventMouseButton) and event.pressed and !mouse_over:
+		InfoLabel.hide()
+
+
+func _on_Provincia_mouse_entered():
+	mouse_over = true
+
+
+func _on_Provincia_mouse_exited():
+	mouse_over = false
